b31bca6c375836b4d2e2c7c7b90c9781ded5911f
Andreu <46297629+AndreuMatoses@users.noreply.github.com>
2025-02-21 17:54:02 +0100

    Update common_pkgs.sh

--- ubuntu_set_up/common_pkgs.sh
+++ ubuntu_set_up/common_pkgs.sh
@@ -3,3 +3,7 @@
 sudo apt install gthumb -y
 sudo apt install vlc -y
 uv tool install poetry
+# for displaying temperatures of CPU and GPU
+sudo apt install psensor -y
+# For clipboard history
+sudo apt install copyq
